6c2aba52b5d5099df73556b2df9df6dacd797dbe
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-05-15 20:02:57 -0500

    (test) Added functional test for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -117,4 +117,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+
 
