41cfc457f29a919da0dd2a5ac00be8ba6a513bda
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-05-15 19:47:42 -0500

    (fix) Modified urls to be namespaced

--- lists/tests.py
+++ lists/tests.py
@@ -13,11 +13,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -54,6 +49,12 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
@@ -72,7 +73,7 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -81,8 +82,7 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
-        self.assertRedirects(response,
-                f'/lists/{new_list.id}/')
+        self.assertRedirects(response,f'/lists/{new_list.id}/')
 
 class NewItemTest(TestCase):
 
@@ -111,8 +111,3 @@ class NewItemTest(TestCase):
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
-        self.assertEqual(response.context['list'], correct_list)
