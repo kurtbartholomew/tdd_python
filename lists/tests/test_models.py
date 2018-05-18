9ebce703d34ce91e2ada155d4aeb2a8e0ca84f70
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-05-18 18:57:03 -0500

    (tests) Added controls to prevent an empty list or list item

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+from django.core.exceptions import ValidationError
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -35,4 +35,12 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+
 
