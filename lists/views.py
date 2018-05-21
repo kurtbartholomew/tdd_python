21c97edefb7bac3020e4e90f96653867d2a5ba57
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-05-21 13:06:55 -0500

    (refactor) Replace all input ids and names

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
+from lists.forms import ItemForm
 from lists.models import Item, List
 
     # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -12,7 +13,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(f'/lists/{list_.id}/')
@@ -23,7 +24,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
     except ValidationError:
